929659fa49c1ddabe2f2d4c3780ee6c97d3dea2a
volcov <volcov.bruno@gmail.com>
2020-08-17 01:52:00 -0300

    add logs to debug

--- lib/lilibot_web/controllers/dominaria.ex
+++ lib/lilibot_web/controllers/dominaria.ex
@@ -1,7 +1,12 @@
 defmodule LilibotWeb.Dominaria do
   use LilibotWeb, :controller
 
-  def raise(conn, _params) do
+  require Logger
+
+  def raise(conn, params) do
+    Logger.info("minha conn -> #{conn}")
+    Logger.info("meus params -> #{params}")
+
     text(conn, Lilictocat.get_oldest_pull_request_without_review())
   end
 end
